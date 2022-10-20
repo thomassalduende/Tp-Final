526bb57b830baa007011bc678b6d97fd97b50184
thomassalduende <thomi44sal@gmail.com>
2022-10-20 12:00:50 -0300

    btn

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,10 +1,13 @@
 import logo from '../assets/logo.png'
 import '../Styles/Header.css'
+import styles from '../Styles/Header.css'
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart, FaUserAlt, FaHouseUser } from "react-icons/fa";
+import { AiTwotoneSetting } from "react-icons/ai";
+import { RiLogoutBoxLine } from "react-icons/ri";
 const Header = () => {
   return ( 
     <nav>
-        
-
         <input type="checkbox" id="check"/>
         <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
@@ -23,7 +26,14 @@ const Header = () => {
             <li class="other-button"><a className="fs-6 fw-bold" href="#">Registrarse</a></li>
             <li><a className="fs-6 fw-bold" href="#">Ingresar</a></li>
             <li><a className="fs-6 fw-bold" href="#">Favoritos</a></li>
+            <li><FaShoppingCart className={styles.iconAnimation}/></li>
+            <li><FaHouseUser className={`${styles.iconAnimation} ${styles.userMenuIn}`}/></li>
+            
         </ul>
+        <li className="searchBar">
+          <input type="text" placeholder="Buscar..." />
+          <FaSearch className="searchBarIcon"/>
+        </li>
     </nav>
 
     
